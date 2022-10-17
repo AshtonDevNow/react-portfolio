d1f44ecdf8b7cbf2585604b6a03e5455f59a4920
Ashton Bunn <ashtonwork@hotmail.com>
2022-10-17 09:01:46 +0800

    Added pacman loader to about page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,4 +1,3 @@
-import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -13,6 +12,8 @@ import {
   faReact,
 } from '@fortawesome/free-brands-svg-icons'
 import { faDatabase, faSheqel } from '@fortawesome/free-solid-svg-icons'
+import Loader from 'react-loaders'
+import './index.scss'
 
 const About = () => {
   // Below is when I can get the useState effect to work.  Need to get
@@ -26,48 +27,51 @@ const About = () => {
   // }, [])
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
-            idx={15}
-          />
-        </h1>
-        <p>Make this about where I'm from and Tech expreience.</p>
-        <p>
-          Here can be about what other industries and countries I worked in.
-          Link resume.
-        </p>
-        <p>
-          This last paragraph can be about how I can adapt to the job and how I
-          use my free time to study Tech.
-        </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>Make this about where I'm from and Tech expreience.</p>
+          <p>
+            Here can be about what other industries and countries I worked in.
+            Link resume.
+          </p>
+          <p>
+            This last paragraph can be about how I can adapt to the job and how
+            I use my free time to study Tech.
+          </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faDatabase} color="#Black" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faDatabase} color="#Black" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
