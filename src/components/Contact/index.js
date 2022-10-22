cd012380e8db699c192538a845f14b01217bbf92
Ashton Bunn <ashtonwork@hotmail.com>
2022-10-22 23:47:45 +0800

    Most emailjs set up

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,16 +2,42 @@ import Loader from 'react-loaders'
 import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
 import { useState } from 'react'
+import emailjs from '@emailjs/browser'
+import { useRef } from 'react'
 
 const Contact = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
-
   // Can't get this effect to work
   // useEffect(() => {
   //   return setTimeout(() => {
   //     setLetterClass('text-animate-hover')
   //   }, 3000)
   // }, [])
+  const [letterClass] = useState('text-animate')
+  // We can prevent blank screen by putting () after the object to allow multiple hooks.
+  const refForm = useRef()
+  // Helps refer to the emailjs service
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs
+      .sendForm(
+        'outlook',
+        'template_oy7pjl5',
+        refForm.current,
+        '3xVDtnXovUkDvKJex'
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!')
+          // avoids reloading the screen
+          window.location.reload(false)
+        },
+        () => {
+          alert('Failed to send the message, please try again')
+        }
+      )
+  }
 
   return (
     <div className="container contact-page">
@@ -25,10 +51,10 @@ const Contact = () => {
         </h1>
         <p>This is to have content about best contact.</p>
         <div className="contact-form">
-          <form>
+          <form ref={refForm} onSubmit={sendEmail}>
             <ul>
               <li>
-                <input type="text" name="name" placeholder="name" required />
+                <input type="text" name="name" placeholder="Name" required />
               </li>
               <li>
                 <input type="text" name="email" placeholder="Email" required />
