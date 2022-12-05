3a25042d0a5be4346950e0db0406aa9ee666457c
AshtonDevNow <84830867+AshtonDevNow@users.noreply.github.com>
2022-12-05 08:38:09 +0800

    Revert "Project page"

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './components/Home'
 import Layout from './components/Layout'
 import About from './components/About'
 import Contact from './components/Contact'
-import Portfolio from './components/Portfolio'
 
 function App() {
   return (
@@ -12,9 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
