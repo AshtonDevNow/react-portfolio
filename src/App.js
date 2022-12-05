fc2534a64a981f2b0ca0ec29f6c91db41e71cd19
Ashton Bunn <ashtonwork@hotmail.com>
2022-12-05 09:42:53 +0800

    icon added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Home from './components/Home'
 import Layout from './components/Layout'
 import About from './components/About'
 import Contact from './components/Contact'
-<<<<<<< HEAD
 // import Portfolio from './components/Portfolio'
-=======
->>>>>>> 0128d5b3ed65b998bbabba2185e149d1533bca78
 
 function App() {
   return (
@@ -15,14 +12,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-<<<<<<< HEAD
           <Route path="/about" element={<About />} />
           {/* <Route path="/portfolio" element={<Portfolio />} /> */}
           <Route path="/contact" element={<Contact />} />
-=======
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
->>>>>>> 0128d5b3ed65b998bbabba2185e149d1533bca78
         </Route>
       </Routes>
     </>
--- src/Portfolio/index.js
+++ src/Portfolio/index.js
@@ -1,28 +1,28 @@
-// import React, { useState } from 'react'
-// import Loader from 'react-loaders'
-// import AnimatedLetters from '../AnimatedLetters'
-// import './index.scss'
-// // import portfolioData from '../../datat/portfoliio.json'
+import React, { useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+// import portfolioData from '../../datat/portfoliio.json'
 
-// const Portfolio = () => {
-//   const [letterClass, setLetterClass] = useState('text-animate')
-//   return (
-//     <>
-//       <div className="container portfolio-page">
-//         <div className="text-zone">
-//           <h1 className="page-title">
-//             <AnimatedLetters
-//               letterClass={letterClass}
-//               strArray={'Portfolio'.split('')}
-//               idx={15}
-//             />
-//           </h1>
-//           {/* <div>{renderPortfolio()}</div> */}
-//         </div>
-//       </div>
-//       <Loader type="pacman" />
-//     </>
-//   )
-// }
+const Portfolio = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  return (
+    <>
+      <div className="container portfolio-page">
+        <div className="text-zone">
+          <h1 className="page-title">
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={'Portfolio'.split('')}
+              idx={15}
+            />
+          </h1>
+          {/* <div>{renderPortfolio()}</div> */}
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
 
-// export default Portfolio
+export default Portfolio
