39ee926d161ab7f40b92f4b1cd1d54593bbca93a
Ashton Bunn <ashtonwork@hotmail.com>
2022-12-27 04:36:19 +0800

    Got layout done for project pages

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,49 +1,53 @@
-import React, { useState } from 'react'
-import Loader from 'react-loaders'
-import AnimatedLetters from '../AnimatedLetters'
-import './index.scss'
-import ProjectItem from './ProjectItem'
-import Football from '../../assets/images/football.png'
-import { ProjectList } from '../../assets/helpers/ProjectList'
+// import React, { useState } from 'react'
+// import Loader from 'react-loaders'
+// import AnimatedLetters from '../AnimatedLetters'
+// import './index.scss'
+// import ProjectItem from './ProjectItem'
+// // import Football from '../../assets/images/football.png'
+// import { ProjectList } from '../../assets/helpers/ProjectList'
 
-const Portfolio = () => {
-  const [letterClass, setLetterClass] = useState('text-animate')
-  return (
-    <>
-      <div className="container portfolio-page">
-        <div className="text-zone">
-          <h1 className="page-title">
-            <AnimatedLetters
-              letterClass={letterClass}
-              strArray={'Portfolio'.split('')}
-              idx={15}
-            />
-          </h1>
-          <p>Here is and overview of the projects.</p>
-          <div className="project">
-            <div className="projectList">
-              {/* <projectItem
-              name="Boredem Bakey"
-              image="../../assets/images/football.png"
-            /> */}
-              <div className="projects">
-                <projectItem name="Giggle" image={Football} />
-                {/* <projectItem name="Equiprent" image={}/> */}
-                <div className="projectList">
-                  {ProjectList.map((project) => {
-                    return (
-                      <ProjectItem name={project.name} image={project.image} />
-                    )
-                  })}
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <Loader type="pacman" />
-    </>
-  )
-}
+// const Portfolio = () => {
+//   const [letterClass, setLetterClass] = useState('text-animate')
+//   return (
+//     <>
+//       <div className="container portfolio-page">
+//         <div className="text-zone">
+//           <h1 className="page-title">
+//             <AnimatedLetters
+//               letterClass={letterClass}
+//               strArray={'Portfolio'.split('')}
+//               idx={15}
+//             />
+//           </h1>
+//           <p>Here is and overview of the projects.</p>
 
-export default Portfolio
+//           <div className="project">
+//             <div className="projectList">
+//               {/* <projectItem
+//               name="Boredem Bakey"
+//               image="../../assets/images/football.png"
+//             /> */}
+//               {/* <projectItem name="Giggle" image={Football}  />
+//                 <projectItem name="Equiprent" image={}/> */}
+
+//               <div className="projectList">
+//                 {ProjectList.map((project, idx) => {
+//                   return (
+//                     <ProjectItem
+//                       id={idx}
+//                       name={project.name}
+//                       image={project.image}
+//                     />
+//                   )
+//                 })}
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//       <Loader type="pacman" />
+//     </>
+//   )
+// }
+
+// export default Portfolio
